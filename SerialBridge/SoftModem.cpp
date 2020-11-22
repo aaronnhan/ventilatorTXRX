89167a7bcc5003e68ca04ac6ad3f03c8e5500df1
Aaron Nhan <aaronnhan11@gmail.com>
2020-11-21 17:13:51 -0800

    added quick read/write methods

--- SerialBridge/SoftModem.cpp
+++ SerialBridge/SoftModem.cpp
@@ -1,9 +1,19 @@
 //#include "SoftModem.h" 
 //
-//#define TX_PIN  (3)
-//#define RX_PIN1 (6)  // AIN0
-//#define RX_PIN2 (7)  // AIN1
+#define TX_PIN  (313
+#define RX_PIN (2)
+//#define RX_PIN2 (7)  // Not Needed?
 //
+
+inline void digitalWriteDirect(int pin, boolean val){
+  if(val) g_APinDescription[pin].pPort -> PIO_SODR = g_APinDescription[pin].ulPin;
+  else    g_APinDescription[pin].pPort -> PIO_CODR = g_APinDescription[pin].ulPin;
+}
+
+inline int digitalReadDirect(int pin){
+  return !!(g_APinDescription[pin].pPort -> PIO_PDSR & g_APinDescription[pin].ulPin);
+}
+
 //SoftModem *SoftModem::activeObject = 0;
 //
 //SoftModem::SoftModem() {
